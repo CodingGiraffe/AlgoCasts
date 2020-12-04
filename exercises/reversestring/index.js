a83d69835f0575d0126ac6a15a1b2c2012f632a9
CodingGiraffe <adamthomas03@gmail.com>
2020-12-04 10:24:24 -0600

    reversed string answers

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+function reverse(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
 
 module.exports = reverse;
