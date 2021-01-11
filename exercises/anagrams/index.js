44df914fbc485782da0fc893e277d4ec09a8daa5
CodingGiraffe <adamthomas03@gmail.com>
2021-01-11 10:00:39 -0600

    completed anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   //pull out the keys and check for extra charaters in map
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   //if same number of characters proceed to checking amounts
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// //build a helper function, do this first
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     //remove white space and punctuation and lowercase all letters
+//     charMap[char] = charMap[char] + 1 || 1; //add one to character or start counting new letter
+//   }
+
+//   return charMap;
+// }
+
+//SOLUTION TWO
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+//make a helper function
+//the sort method will put all characters in alphabetical order to compare, only used on array.
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
